5f9c436c8d8b9184a76e4338201d04bca1d1e9b7
tttinnny <tttinnny@gmail.com>
2022-01-15 14:29:19 +0300

    add share! macro for sharing entities to other entities. add BrickMultyUsege for safe sharing entity between some other entity. add impl call method for InterruptSplitter that perform execution of all callbacks that it has. add impl set_splitter method for InterruptSplitter that set splitter via a little bit convenient form. delete unnecessary BrickBase and now bricks will hold only InterruptSplitter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,59 @@
-#![feature(const_fn_fn_ptr_basics)]
-#![feature(const_fn_trait_bound)]
-//#![feature(const_trait_impl)]
 #![no_std]
 
-use core::marker::PhantomData;
-//use core::borrow::{BorrowMut, Borrow};
 pub use gestalt_reference_api::interface::GenericInterface;
 
-/// Brick body, that include in itself:
-/// generic status (u should use enums, but it possible use other entities),
-/// Data, that has generic type for storing and working with any data.
-/// EXT_FN_NB - number interrupt, that Brick can listen.
-/// EXT_EVT_NB - number interrupt, that Brick can give to other Bricks
-
+#[derive(Clone, Copy)]
 pub struct InterruptPlug{}
 
-impl <T>CallbackExec<T> for InterruptPlug { fn call(&mut self) {} }
+pub static global_interrupt_plug: InterruptPlug = InterruptPlug{};
 
-pub struct InterruptSplitter<'a, T: ?Sized>
+impl <T> BrickCallbackImpl<T> for InterruptPlug { fn call(&mut self) {} }
+
+pub struct InterruptSplitter<T: ?Sized>
 {
-    pub splitter_ouput_interrupt: &'a mut [&'a mut T]
+    pub splitter_ouput_interrupt: Option<* mut [* mut dyn BrickCallbackImpl<T>]>
 }
 
-pub struct BrickBase<'a, T_2, STATUS, DATA, const INTERRUPT_NUMB: usize>
-{
-    pub status: STATUS,
-    pub data: DATA,
-    pub output_interrupt: [InterruptSplitter<'a, CallbackExec<T_2>>; INTERRUPT_NUMB]
-}
-
-
-pub trait PeripheralNew <T_2, CALL: CallbackExec<T_2>, T, INST, const NUMB: usize> {
+pub trait PeripheralNew <T_2, CALL: BrickCallbackImpl<T_2>, T, INST, const NUMB: usize> {
     fn new (inst: INST) -> & 'static mut T;
     fn _new(inst: INST, interrupt_arguments: [&mut CALL; NUMB]) -> T;
 }
 
-pub trait CallbackExec<T>{
+pub trait BrickCallbackImpl<T: ?Sized>{
     fn call(&mut self);
 }
 
-
-pub trait  Default<STATUS>
+pub trait BrickPollImpl
 {
-    fn default() -> STATUS;
-}
-
-pub trait BrickExternImpl
-{
-    fn brick_main(&mut self);
     fn poll (&mut self);
+}
+
+pub trait SplitterImpl<T> {
+    fn set_splitter(&mut self, splitter: * mut [* mut dyn BrickCallbackImpl<T>]);
+}
+
+impl <T: ?Sized> InterruptSplitter<T> {
+    pub fn set_splitter(&mut self, splitter: * mut [* mut dyn BrickCallbackImpl<T>]){
+        self.splitter_ouput_interrupt = Some(splitter);
+    }
 
 }
 
-// impl <STATUS: Clone, DATA>
-//     BrickBaseImpl<STATUS, DATA> for
-//     BrickBase<STATUS, DATA>
-// {
-//     fn get_status(&mut self) -> STATUS { self.local_status.clone() }
-// }
+impl <T>InterruptSplitter<T> {
+    pub fn call(&mut self) { unsafe {
+            (*self.splitter_ouput_interrupt.unwrap()).iter().for_each( |&this| (*this).call());
+        }
+    }
+}
+
+pub trait BrickMultyUsege {
+    fn request_access(&mut self);
+    fn free(&mut self);
+}
+
+#[macro_export]
+macro_rules! share
+{
+    (ptr, $el:ident, $T:ty) =>  { unsafe { (&$el as * const $T) as * mut $T } };
+    (mref, $el:ident, $T:ty) => { unsafe { &mut *((&$el as * const $T) as * mut $T) } };
+}
