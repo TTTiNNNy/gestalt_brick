ec5e75f2e33547f4b3fd4e735f1e4dd5a08fdb8c
tttinnny <tttinnny@gmail.com>
2022-01-15 14:40:42 +0300

    fix some code warnings. Remove gestalt_reference_api unnecessary dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 #![no_std]
 
-pub use gestalt_reference_api::interface::GenericInterface;
 
 #[derive(Clone, Copy)]
 pub struct InterruptPlug{}
 
-pub static global_interrupt_plug: InterruptPlug = InterruptPlug{};
+pub static GLOBAL_INTERRUPT_PLUG: InterruptPlug = InterruptPlug{};
 
 impl <T> BrickCallbackImpl<T> for InterruptPlug { fn call(&mut self) {} }
 
@@ -14,9 +13,9 @@ pub struct InterruptSplitter<T: ?Sized>
     pub splitter_ouput_interrupt: Option<* mut [* mut dyn BrickCallbackImpl<T>]>
 }
 
-pub trait PeripheralNew <T_2, CALL: BrickCallbackImpl<T_2>, T, INST, const NUMB: usize> {
-    fn new (inst: INST) -> & 'static mut T;
-    fn _new(inst: INST, interrupt_arguments: [&mut CALL; NUMB]) -> T;
+pub trait PeripheralNew <AnyType, Call: BrickCallbackImpl<AnyType>, T, Inst, const NUMB: usize> {
+    fn new (inst: Inst) -> & 'static mut T;
+    fn _new(inst: Inst, interrupt_arguments: [&mut Call; NUMB]) -> T;
 }
 
 pub trait BrickCallbackImpl<T: ?Sized>{
